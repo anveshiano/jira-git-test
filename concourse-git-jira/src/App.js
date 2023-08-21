63c8832733f3436cab2130b7db6d2a8512c7ebd4
anveshiano <anvesh021@gmail.com>
2023-08-21 03:09:17 +0100

    JGIT-2 Smartcommit 4 test

--- concourse-git-jira/src/App.js
+++ concourse-git-jira/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>test change two</p>
+        <p>test change THREE</p>
         <a
           className="App-link"
           href="https://reactjs.org"
